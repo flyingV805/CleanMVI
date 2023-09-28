42a7b9fc0b02441040c5a954a17db9daa3bf0f1b
flyingV805 <petrov.vladislav1992@gmail.com>
2023-09-28 22:00:50 +0600

    TimeMachine feature started

--- cleanmvi/src/main/java/kz/flyingv/cleanmvi/UIViewModel.kt
+++ cleanmvi/src/main/java/kz/flyingv/cleanmvi/UIViewModel.kt
@@ -1,11 +1,16 @@
 package kz.flyingv.cleanmvi
 
+import androidx.annotation.CallSuper
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kz.flyingv.cleanmvi.tools.timemachine.TimeMachine
 
-abstract class UIViewModel<S : UIState, in A : UIAction>(initialState: S): ViewModel() {
+abstract class UIViewModel<S : UIState, in A : UIAction>(
+    initialState: S,
+    private val timeMachine: TimeMachine<S, A>? = null
+): ViewModel() {
 
     private val _uiState = MutableStateFlow(initialState)
 
@@ -14,9 +19,13 @@ abstract class UIViewModel<S : UIState, in A : UIAction>(initialState: S): ViewM
     protected fun currentState(): S = _uiState.value
 
     protected fun pushState(newState: S){
+        timeMachine?.stateEmitted(newState)
         _uiState.tryEmit(newState)
     }
 
-    abstract fun reduce(action: A)
+    @CallSuper
+    open fun reduce(action: A){
+        timeMachine?.actionMade(action)
+    }
 
 }
--- example/src/main/java/kz/flyingv/cleanmvi/showcase/feature/ui/FeatureViewModel.kt
+++ example/src/main/java/kz/flyingv/cleanmvi/showcase/feature/ui/FeatureViewModel.kt
@@ -6,20 +6,24 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kz.flyingv.cleanmvi.UIViewModel
 import kz.flyingv.cleanmvi.showcase.feature.domain.usecase.FeatureUseCase
+import kz.flyingv.cleanmvi.tools.timemachine.TimeMachineProvider
 import javax.inject.Inject
 
 @HiltViewModel
 class FeatureViewModel @Inject constructor(
     private val useCase: FeatureUseCase
-): UIViewModel<FeatureState, FeatureAction>(initialState = FeatureState()) {
-
+): UIViewModel<FeatureState, FeatureAction>(
+    initialState = FeatureState(),
+    timeMachine = TimeMachineProvider<FeatureState, FeatureAction>().default()
+) {
 
     override fun reduce(action: FeatureAction) {
+        super.reduce(action)
         when(action){
             is FeatureAction.UpdateFact -> {
                 pushState(currentState().copy(isLoading = true))
                 viewModelScope.launch(Dispatchers.IO) {
-                    val newFact = useCase.invoke()
+                    val newFact = useCase()
                     pushState(currentState().copy(
                         fact = newFact?.factContent,
                         isLoading = false
@@ -27,7 +31,6 @@ class FeatureViewModel @Inject constructor(
                 }
             }
         }
-
 
     }
 
